a7b644876a3cdfb190b85aba6a81d6edf2154438
student <s530748@ch112015m.nwmissouri.edu>
2018-03-01 11:34:41 -0600

    Initial Commit

---
+++ Potukuru_SexretCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Potukuru_SexretCode
+//
+//  Created by Potukuru,Durga Charan on 3/1/18.
+//  Copyright © 2018 Potukuru,Durga Charan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
