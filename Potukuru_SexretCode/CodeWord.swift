b37a403d9b5f10fdbf39bd7def6ee2f5e82ea2a8
Potukuru <s530748@ch112015m.nwmissouri.edu>
2018-03-01 12:18:16 -0600

    first work

---
+++ Potukuru_SexretCode/CodeWord.swift
@@ -0,0 +1,24 @@
+//
+//  CodeWord.swift
+//  Potukuru_SexretCode
+//
+//  Created by Potukuru,Durga Charan on 3/1/18.
+//  Copyright © 2018 Potukuru,Durga Charan. All rights reserved.
+//
+
+import Foundation
+
+class CodeWord {
+    var Symbols = [String]()
+    var Count : Int
+    var Code = [String]()
+    var Guess = [String]()
+    var OnSymbol :Int
+    var Attempts : Int = 31
+    var Status : String
+    
+    init(sofcode : Int){
+        
+    }
+
+}
